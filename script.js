5ad77b3b5a96fa33ee7fbe102defcf88b98c213c
SeavenBlue <83270098+SeavenBlue@users.noreply.github.com>
2022-04-07 09:37:18 +0200

    trying a tutorial

--- script.js
+++ script.js
@@ -1,52 +1,55 @@
 const imageUpload = document.getElementById('imageUpload')
 
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-]).then(start)
+  // load the models
+  faceapi.loadMtcnnModel('/models/'),
+   faceapi.loadFaceRecognitionModel('/models/')
+]).then(startVid())
 
-async function start() {
-  const container = document.createElement('div')
-  container.style.position = 'relative'
-  document.body.append(container)
-  const labeledFaceDescriptors = await loadLabeledImages()
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-  let image
-  let canvas
-  document.body.append('Loaded')
-  imageUpload.addEventListener('change', async () => {
-    if (image) image.remove()
-    if (canvas) canvas.remove()
-    image = await faceapi.bufferToImage(imageUpload.files[0])
-    container.append(image)
-    canvas = faceapi.createCanvasFromMedia(image)
-    container.append(canvas)
-    const displaySize = { width: image.width, height: image.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-    results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-      drawBox.draw(canvas)
-    })
-  })
-}
 
-function loadLabeledImages() {
-  const labels = ['Gustav','Thomas']
-  return Promise.all(
-    labels.map(async label => {
-      const descriptions = []
-      for (let i = 1; i <= 5; i++) { 
-        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/SeavenBlue/Fravaers-registrering/master/labeled_images/${label}/${i}.jpg`)
-        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-        descriptions.push(detections.descriptor)
-      }
 
-      return new faceapi.LabeledFaceDescriptors(label, descriptions)
-    })
+
+async function startVid() {
+  // try to access users webcam and stream the images
+  // to the video element
+  const videoEl = document.getElementById('inputVideo')
+  navigator.getUserMedia(
+    { video: {} },
+    stream => videoEl.srcObject = stream,
+    err => console.error(err)
   )
+
 }
+
+
+
+async function onPlay(){
+   
+  const mtcnnForwardParams = {
+    // number of scaled versions of the input image passed through the CNN
+    // of the first stage, lower numbers will result in lower inference time,
+    // but will also be less accurate
+    maxNumScales: 10,
+    // scale factor used to calculate the scale steps of the image
+    // pyramid used in stage 1
+    scaleFactor: 0.709,
+    // the score threshold values used to filter the bounding
+    // boxes of stage 1, 2 and 3
+    scoreThresholds: [0.6, 0.7, 0.7],
+    // mininum face size to expect, the higher the faster processing will be,
+    // but smaller faces won't be detected
+    minFaceSize: 200
+  }
+  const mtcnnResults = await faceapi.mtcnn(document.getElementById('inputVideo'), mtcnnForwardParams)
+
+
+
+const options = new faceapi.MtcnnOptions(mtcnnForwardParams)
+const input = document.getElementById('inputVideo')
+const fullFaceDescriptions = await faceapi.detectAllFaces(input, options).withFaceLandmarks().withFaceDescriptors()
+
+
+  }
+
+ 
+
